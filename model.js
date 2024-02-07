3be772348d95da1a170fc0a05fe17370fe1f0cba
pritamkoyari <79264442+pritamkoyari@users.noreply.github.com>
2024-02-07 21:05:22 +0530

    Add files via upload

--- model.js
+++ model.js
@@ -1,23 +1,20 @@
 $(document).ready(function () {
-  var audio = new Audio('audio.mp3'); 
+    $(".container")
+      .mouseenter(function () {
+        $(".card").stop().animate(
+          {
+            top: "-90px"
+          },
+          "slow"
+        );
+      })
+      .mouseleave(function () {
+        $(".card").stop().animate(
+          {
+            top: 0
+          },
+          "slow"
+        );
+      });
+  });
   
-  $(".container")
-    .mouseenter(function () {
-      $(".card").stop().animate(
-        {
-          top: "-90px"
-        },
-        "slow"
-      );
-      audio.play(); 
-    })
-    .mouseleave(function () {
-      $(".card").stop().animate(
-        {
-          top: 0
-        },
-        "slow"
-      );
-      audio.pause();
-    });
-});
