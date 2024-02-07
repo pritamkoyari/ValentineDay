e604384a932569dc098d5d283732d6e89b236413
pritamkoyari <79264442+pritamkoyari@users.noreply.github.com>
2024-02-07 21:13:46 +0530

    Update model.js

--- model.js
+++ model.js
@@ -7,7 +7,30 @@ $(document).ready(function () {
           },
           "slow"
         );
-      })
+      })$(document).ready(function () {
+  var audio = new Audio('audio.mp3'); 
+  
+  $(".container")
+    .mouseenter(function () {
+      $(".card").stop().animate(
+        {
+          top: "-90px"
+        },
+        "slow"
+      );
+      audio.play(); 
+    })
+    .mouseleave(function () {
+      $(".card").stop().animate(
+        {
+          top: 0
+        },
+        "slow"
+      );
+      audio.pause();
+    });
+});
+
       .mouseleave(function () {
         $(".card").stop().animate(
           {
