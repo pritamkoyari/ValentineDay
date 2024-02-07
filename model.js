7d12896bb0757f6ffa42d418d3b108cd272029e1
pritamkoyari <pritamkoyari@gmail.com>
2024-02-07 21:02:18 +0530

    [UPD]

--- model.js
+++ model.js
@@ -1,20 +1,23 @@
 $(document).ready(function () {
-    $(".container")
-      .mouseenter(function () {
-        $(".card").stop().animate(
-          {
-            top: "-90px"
-          },
-          "slow"
-        );
-      })
-      .mouseleave(function () {
-        $(".card").stop().animate(
-          {
-            top: 0
-          },
-          "slow"
-        );
-      });
-  });
+  var audio = new Audio('audio.mp3'); 
   
+  $(".container")
+    .mouseenter(function () {
+      $(".card").stop().animate(
+        {
+          top: "-90px"
+        },
+        "slow"
+      );
+      audio.play(); 
+    })
+    .mouseleave(function () {
+      $(".card").stop().animate(
+        {
+          top: 0
+        },
+        "slow"
+      );
+      audio.pause();
+    });
+});
